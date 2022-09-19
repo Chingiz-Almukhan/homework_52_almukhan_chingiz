08115ce6321f99db76370a888daff48e9b63aa2d
Almukhan Chingiz <chingizpain@gmail.com>
2022-09-20 04:16:43 +0600

    Был создан метод, который рендерил главную страницу

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from todoapp.models import Article
+
+
+def index_view(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'main_page.html', context)
