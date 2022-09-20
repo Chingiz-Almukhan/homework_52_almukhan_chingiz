87c40319253e5374fffff75c38202f7ceb544ed0
Almukhan Chingiz <chingizpain@gmail.com>
2022-09-20 08:23:27 +0600

    Добавлены пути для перехода на страницу редактирования

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from todoapp.views import index_view, add_view, add
+from todoapp.views import index_view, add_view, add, edit_view, edit, confirm_edit
 
 urlpatterns = [
     path('', index_view),
     path('add/', add_view),
-    path('add/add_to_list/', add)
+    path('add/add_to_list/', add),
+    path('edit/', edit_view),
+    path('edit/confirm/', edit),
+    path('edit/confirm/edit/', confirm_edit)
 
 ]
