1f98eadc7f446a32e76f43eacfb58c43b211062d
Almukhan Chingiz <chingizpain@gmail.com>
2022-09-20 09:14:04 +0600

    Добавлен путь для перехода к методам для удаления

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
-from todoapp.views import index_view, add_view, add, edit_view, edit, confirm_edit
+from todoapp.views import index_view, add_view, add, edit_view, edit, confirm_edit, delete_view, delete
 
 urlpatterns = [
     path('', index_view),
     path('add/', add_view),
-    path('add/add_to_list/', add),
+    path("add/add_to_list/", add),
     path('edit/', edit_view),
     path('edit/confirm/', edit),
-    path('edit/confirm/edit/', confirm_edit)
-
+    path('edit/confirm/edit/', confirm_edit),
+    path('delete/', delete_view),
+    path('delete/confirm/', delete)
 ]
