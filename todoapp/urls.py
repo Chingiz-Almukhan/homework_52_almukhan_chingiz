22a8119e8b18207d1c3f31af5fa2c42b2e8b84c5
Almukhan Chingiz <chingizpain@gmail.com>
2022-09-20 06:55:17 +0600

    Добавлен путь, который ведет к методу добавления задачи

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from todoapp.views import index_view, add_view
+from todoapp.views import index_view, add_view, add
 
 urlpatterns = [
     path('', index_view),
     path('add/', add_view),
+    path('add/add_to_list/', add)
 
 ]
