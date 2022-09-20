fefdb0543d2ef2daeb9341f6d98c28f9f389e8db
Almukhan Chingiz <chingizpain@gmail.com>
2022-09-20 06:56:36 +0600

    Создан метод для получения значении с post запроса, после чего идет добавление в бд и переход на главную страницу

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from todoapp.models import Article
 
@@ -12,7 +12,13 @@ def index_view(request):
 
 
 def add_view(request):
-    choices = (('new', 'Новая'), ('in process', 'В процессе'), ('ready', 'Сделано'))
-
     return render(request, 'add_task.html', context={'statuses': [('new', 'Новая'), ('in process', 'В процессе'),
                                                                   ('ready', 'Сделано')]})
+
+
+def add(request):
+    description = request.POST.get('description')
+    status = request.POST.get('status')
+    deadline = request.POST.get('deadline')
+    article = Article.objects.create(description=description, status=status, deadline=deadline)
+    return redirect('http://127.0.0.1:8000/')
